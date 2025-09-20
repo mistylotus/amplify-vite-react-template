98dacb07b4b0949e8b7c8e6b931c2a68a345a4de
Mitsuteru Sakashita <sakashita.mitsuteru@gmail.com>
2025-09-20 18:03:11 +0900

    added per-user data isolation

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,8 @@ function App() {
   
   return (
     <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>      <button onClick={createTodo}>+ new</button>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+      <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
           <li 
